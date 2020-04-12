6ecd0e943b9b86c9c75d51a30a8a55232689c4a2
matthiashermsen <matthiashermsen@gmail.com>
2020-04-12 23:21:19 +0200

    location entity

--- src/locations/location.entity.ts
+++ src/locations/location.entity.ts
@@ -1,4 +1,43 @@
-import { Entity, BaseEntity } from 'typeorm';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+import { Min, Max } from 'class-validator';
+
+import { LocationFillingLevel } from './locationFillingLevel.enum';
 
 @Entity()
-export class Location extends BaseEntity {}
+export class Location extends BaseEntity {
+  @PrimaryColumn({
+    type: 'real',
+  })
+  @Min(-90)
+  @Max(90)
+  latitude: number;
+
+  @PrimaryColumn({
+    type: 'real',
+  })
+  @Min(-180)
+  @Max(180)
+  longitude: number;
+
+  @Column()
+  public description: string;
+
+  @Column({
+    type: 'enum',
+    enum: LocationFillingLevel,
+  })
+  public fillingLevel: LocationFillingLevel;
+
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  @UpdateDateColumn()
+  public updatedAt: Date;
+}
---
+++ src/locations/locationFillingLevel.enum.ts
@@ -0,0 +1,5 @@
+export enum LocationFillingLevel {
+    FULL = 'FULL',
+    HALF_FULL = 'HALF_FULL',
+    EMPTY = 'EMPTY',
+}
