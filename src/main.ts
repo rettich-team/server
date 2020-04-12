21f89a12835cfd214bc95855d274a62421b1e4bf
matthiashermsen <matthiashermsen@gmail.com>
2020-04-12 12:40:39 +0200

    added missing type for app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { ServerConfigurationService } from './configurations/server.configuration.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
   const serverConfigurationService: ServerConfigurationService = app.get(ServerConfigurationService);
   const serverPort: number = serverConfigurationService.port;
 
