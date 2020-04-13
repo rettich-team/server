62efa7c0b09008a34eb553751b40682ea875447f
matthiashermsen <matthiashermsen@gmail.com>
2020-04-13 08:27:41 +0200

    inject locations repository into locations service

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { LocationsRepository } from './locations.repository';
 
 @Injectable()
-export class LocationsService {}
+export class LocationsService {
+    constructor(
+        @InjectRepository(LocationsRepository)
+        private readonly locationsRepository: LocationsRepository
+    ) {}
+}
