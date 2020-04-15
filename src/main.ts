b63c107c101f7b61da8e0870071e854e5d07009e
matthiashermsen <matthiashermsen@gmail.com>
2020-04-15 09:43:28 +0200

    #3 swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { ServerConfigurationService } from './configurations/server.configuration.service';
+import { SwaggerConfigurationService } from './configurations/swagger.configuration.service';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
   const serverConfigurationService: ServerConfigurationService = app.get(ServerConfigurationService);
-  const serverPort: number = serverConfigurationService.port;
+  const swaggerConfigurationService: SwaggerConfigurationService = app.get(SwaggerConfigurationService);
+  
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle(swaggerConfigurationService.title)
+    .setDescription(swaggerConfigurationService.description)
+    .setVersion(swaggerConfigurationService.version)
+    .build();
+  const swaggerDocument: OpenAPIObject = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup(swaggerConfigurationService.path, app, swaggerDocument);
 
   app.enableCors();
 
@@ -17,7 +27,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(serverPort);
+  await app.listen(serverConfigurationService.port);
 }
 
 bootstrap();
