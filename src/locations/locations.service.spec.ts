18988d95a71d3d0eea12c53a5d6e866caa7bc5b7
matthiashermsen <matthiashermsen@gmail.com>
2020-04-18 22:55:05 +0200

    #11 added service tests

--- src/locations/locations.service.spec.ts
+++ src/locations/locations.service.spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotFoundException } from '@nestjs/common';
+import { NotFoundException, ConflictException } from '@nestjs/common';
 
 import { LocationsService } from './locations.service';
 import { LocationsRepository } from './locations.repository';
 import { Location } from './location.entity';
 import { LocationFillingLevel } from './locationFillingLevel.enum';
+import { AddLocationDTO } from './dtos/addLocation.dto';
 
 const mockLocationsRepository = () => ({
   getLocations: jest.fn(),
   getLocation: jest.fn(),
+  addLocation: jest.fn(),
 });
 
 describe('LocationsService', () => {
@@ -30,33 +32,30 @@ describe('LocationsService', () => {
     locationsRepository = module.get<LocationsRepository>(LocationsRepository);
   });
 
+  const mockLocation: Location = {
+    latitude: 0,
+    longitude: 0,
+    description: '',
+    fillingLevel: LocationFillingLevel.EMPTY,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    hasId: null,
+    save: null,
+    remove: null,
+    reload: null,
+  };
+
   it('should be defined', () => {
     expect(locationsService).toBeDefined();
   });
 
   describe('getLocation', () => {
     it('returns a location by coordinates', async () => {
-      const latitude = 0;
-      const longitude = 0;
-      
-      const mockLocation: Location = {
-        latitude,
-        longitude,
-        description: '',
-        fillingLevel: LocationFillingLevel.EMPTY,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        hasId: null,
-        save: null,
-        remove: null,
-        reload: null,
-      };
-
       jest
         .spyOn(locationsRepository, 'getLocation')
         .mockResolvedValue(mockLocation);
 
-      const location: Location = await locationsService.getLocation(latitude, longitude);
+      const location: Location = await locationsService.getLocation(mockLocation.latitude, mockLocation.longitude);
 
       expect(location).toEqual(mockLocation);
     });
@@ -67,6 +66,33 @@ describe('LocationsService', () => {
         .mockResolvedValue(null);
 
       expect(locationsService.getLocation(0, 0)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('addLocation', () => {
+    const addLocationDTO: AddLocationDTO = { 
+      latitude: 0, 
+      longitude: 0, 
+      description: ''
+    };
+
+    it('adds a location', async () => {
+      jest
+        .spyOn(locationsRepository, 'addLocation')
+        .mockResolvedValue(mockLocation);
+
+      const location: Location = await locationsService.addLocation(addLocationDTO);
+
+      expect(locationsRepository.addLocation).toHaveBeenCalledWith(addLocationDTO);
+      expect(location).toEqual(mockLocation);
+    });
+
+    it('throws a ConflictException if the location already exists', () => {
+      jest
+        .spyOn(locationsRepository, 'getLocation')
+        .mockResolvedValue(mockLocation);
+
+      expect(locationsService.addLocation(addLocationDTO)).rejects.toThrow(ConflictException);
     });
   });
 });
