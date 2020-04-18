6682d510a3ff32ebe20ac9bcea648dbefc495da3
matthiashermsen <matthiashermsen@gmail.com>
2020-04-18 17:28:09 +0200

    #7 added tests

--- src/loaders/typeORM.loader.spec.ts
+++ src/loaders/typeORM.loader.spec.ts
@@ -1,10 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { TypeOrmLoader } from './typeORM.loader';
 import { DatabaseConfigurationService } from '../configurations/database.configuration.service';
 
-const mockDatabaseConfigurationService = () => ({
-});
+const mockDatabaseConfiguration: object = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'admin',
+  database: 'db',
+  entities: [],
+  logging: false,
+  synchronize: false,
+};
+
+const mockDatabaseConfigurationService = () => (mockDatabaseConfiguration);
 
 describe('TypeOrmLoader', () => {
   let typeOrmLoader: TypeOrmLoader;
@@ -25,5 +37,11 @@ describe('TypeOrmLoader', () => {
 
   it('should be defined', () => {
     expect(typeOrmLoader).toBeDefined();
+  });
+
+  it('should return a TypeORM configuration object', () => {
+    const configurationOptions: TypeOrmModuleOptions = typeOrmLoader.createTypeOrmOptions();
+
+    expect(configurationOptions).toMatchObject(mockDatabaseConfiguration);
   });
 });
