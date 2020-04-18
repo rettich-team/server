97265542cb1d06644a25d990fad0be4164b37b2c
matthiashermsen <matthiashermsen@gmail.com>
2020-04-18 22:05:38 +0200

    #12 added controller and service tests

--- src/locations/locations.controller.spec.ts
+++ src/locations/locations.controller.spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
+import { Location } from './location.entity';
+import { LocationFillingLevel } from './locationFillingLevel.enum';
 
 const mockLocationsService = () => ({
   getLocations: jest.fn(),
+  getLocation: jest.fn(),
 });
 
 describe('LocationsController', () => {
@@ -28,5 +31,33 @@ describe('LocationsController', () => {
 
   it('should be defined', () => {
     expect(locationsController).toBeDefined();
+  });
+
+  describe('getLocation', () => {
+    it('returns a location by coordinates', async () => {
+      const latitude = 0;
+      const longitude = 0;
+      
+      const mockLocation: Location = {
+        latitude,
+        longitude,
+        description: '',
+        fillingLevel: LocationFillingLevel.EMPTY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        hasId: null,
+        save: null,
+        remove: null,
+        reload: null,
+      };
+
+      jest
+        .spyOn(locationsService, 'getLocation')
+        .mockResolvedValue(mockLocation);
+
+      const location: Location = await locationsController.getLocation({ latitude, longitude });
+
+      expect(location).toEqual(mockLocation);
+    });
   });
 });
--- src/locations/locations.service.spec.ts
+++ src/locations/locations.service.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 
 import { LocationsService } from './locations.service';
 import { LocationsRepository } from './locations.repository';
+import { Location } from './location.entity';
+import { LocationFillingLevel } from './locationFillingLevel.enum';
 
 const mockLocationsRepository = () => ({
   getLocations: jest.fn(),
+  getLocation: jest.fn(),
 });
 
 describe('LocationsService', () => {
@@ -28,5 +32,41 @@ describe('LocationsService', () => {
 
   it('should be defined', () => {
     expect(locationsService).toBeDefined();
+  });
+
+  describe('getLocation', () => {
+    it('returns a location by coordinates', async () => {
+      const latitude = 0;
+      const longitude = 0;
+      
+      const mockLocation: Location = {
+        latitude,
+        longitude,
+        description: '',
+        fillingLevel: LocationFillingLevel.EMPTY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        hasId: null,
+        save: null,
+        remove: null,
+        reload: null,
+      };
+
+      jest
+        .spyOn(locationsRepository, 'getLocation')
+        .mockResolvedValue(mockLocation);
+
+      const location: Location = await locationsService.getLocation(latitude, longitude);
+
+      expect(location).toEqual(mockLocation);
+    });
+
+    it('throws a NotFoundException if the location was not found', () => {
+      jest
+        .spyOn(locationsRepository, 'getLocation')
+        .mockResolvedValue(null);
+
+      expect(locationsService.getLocation(0, 0)).rejects.toThrow(NotFoundException);
+    });
   });
 });
