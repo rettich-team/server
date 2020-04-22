5eb8ff6d72b26e7765c92f774327861e6db696b0
matthiashermsen <matthiashermsen@gmail.com>
2020-04-22 13:45:53 +0200

    extended schema validator

--- src/shared/validators/valueSchemaValidation.service.spec.ts
+++ src/shared/validators/valueSchemaValidation.service.spec.ts
@@ -27,4 +27,22 @@ describe('ValueSchemaValidationService', () => {
   it('throws a ValidationError on invalid configuration', () => {
     expect(() => valueSchemaValidationService.validateValue('text', Joi.number(), 'text')).toThrow(Joi.ValidationError);
   });
+
+  it('starts the ValidationError message with the "valueName" if provided', () => {
+    const valueName = 'fieldName';
+    
+    try {
+      valueSchemaValidationService.validateValue('text', Joi.number(), valueName)
+    } catch (error) {
+      expect(error.message.startsWith(`"${valueName}"`)).toBeTruthy();
+    }
+  });
+
+  it('starts the ValidationError message with "value" if no value name was provided', () => {
+    try {
+      valueSchemaValidationService.validateValue('text', Joi.number())
+    } catch (error) {
+      expect(error.message.startsWith('"value"')).toBeTruthy();
+    }
+  });
 });
--- src/shared/validators/valueSchemaValidation.service.ts
+++ src/shared/validators/valueSchemaValidation.service.ts
@@ -3,9 +3,12 @@ import { AnySchema, ValidationResult, ValidationError } from '@hapi/joi';
 
 @Injectable()
 export class ValueSchemaValidationService {
-    public validateValue<TValue>(value: TValue, validator: AnySchema, property: string): void {
-        const validationSchema: AnySchema = validator.label(property);
-        const validationResult: ValidationResult = validationSchema.validate(value);
+    public validateValue<TValue>(value: TValue, validator: AnySchema, valueName?: string): void {
+        if (valueName) {
+            validator = validator.label(valueName);
+        }
+        
+        const validationResult: ValidationResult = validator.validate(value);
         const validationError: ValidationError = validationResult.error;
 
         if (validationError) {
