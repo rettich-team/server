43dfb36cc7286de1aa2baa759089742b171d55a6
matthiashermsen <matthiashermsen@gmail.com>
2020-04-24 22:02:25 +0200

    #14 extended dto for search radius

--- src/locations/dtos/getLocations.dto.ts
+++ src/locations/dtos/getLocations.dto.ts
@@ -1,8 +1,9 @@
-import { IsOptional, IsEnum, IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { IsOptional, IsEnum, IsNotEmpty, IsString, IsInt, IsNumber, IsLatitude, IsLongitude, ValidateIf } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 import { LocationFillingLevel } from '../locationFillingLevel.enum';
+import { IsDoublePrecision } from '../../shared/decorators/isDoublePrecision.decorator';
 
 export class GetLocationsDTO {
     @IsEnum(LocationFillingLevel)
@@ -15,6 +16,28 @@ export class GetLocationsDTO {
     @IsOptional()
     @ApiProperty()
     public partialDescription?: string;
+
+    @IsNumber()
+    @IsDoublePrecision()
+    @IsLatitude()
+    @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.longitude !== undefined || getLocationsDTO.longitude !== undefined)
+    @Type(() => Number)
+    @ApiProperty()
+    public latitude?: number;
+
+    @IsNumber()
+    @IsDoublePrecision()
+    @IsLongitude()
+    @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.latitude !== undefined || getLocationsDTO.radiusInKilometers !== undefined)
+    @Type(() => Number)
+    @ApiProperty()
+    public longitude?: number;
+
+    @IsNumber()
+    @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.latitude !== undefined || getLocationsDTO.longitude !== undefined)
+    @Type(() => Number)
+    @ApiProperty()
+    public radiusInKilometers?: number;
 
     @IsInt()
     @IsOptional()
