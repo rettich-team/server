736b1732e66e799d37b2e6b71ddac3fade8bace0
matthiashermsen <matthiashermsen@gmail.com>
2020-04-26 13:23:24 +0200

    #18

--- src/locations/dtos/updateLocationDescription/updateLocationDescription.body.dto.ts
+++ src/locations/dtos/updateLocationDescription/updateLocationDescription.body.dto.ts
@@ -5,5 +5,11 @@ export class UpdateLocationDescriptionBodyDTO {
     @IsString()
     @MaxLength(Number(process.env.LOCATION_DESCRIPTION_MAX_LENGTH))
     @ApiProperty()
+    @ApiProperty({
+        name: 'description',
+        required: true,
+        description: `The description of a location up to ${process.env.LOCATION_DESCRIPTION_MAX_LENGTH} characters.`,
+        type: String
+    })
     public description: string;
 }
