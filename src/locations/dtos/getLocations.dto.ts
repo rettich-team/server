4703c9b06620cfa499d41d159c97adac248c2edc
matthiashermsen <matthiashermsen@gmail.com>
2020-04-26 13:49:27 +0200

    #18

--- src/locations/dtos/getLocations.dto.ts
+++ src/locations/dtos/getLocations.dto.ts
@@ -8,21 +8,38 @@ import { IsDoublePrecision } from '../../shared/decorators/isDoublePrecision.dec
 export class GetLocationsDTO {
     @IsEnum(LocationFillingLevel)
     @IsOptional()
-    @ApiProperty()
+    @ApiProperty({
+        name: 'fillingLevel',
+        required: false,
+        description: `The filling level of a location.`,
+        type: LocationFillingLevel,
+        enum: LocationFillingLevel,
+        enumName: 'LocationFillingLevel',
+    })
     public fillingLevel?: LocationFillingLevel;
 
     @IsString()
     @IsNotEmpty()
     @IsOptional()
-    @ApiProperty()
+    @ApiProperty({
+        name: 'partialDescription',
+        required: false,
+        description: `Check if the location description contains this substring.`,
+        type: String,
+    })
     public partialDescription?: string;
 
     @IsNumber()
     @IsDoublePrecision()
     @IsLatitude()
-    @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.longitude !== undefined || getLocationsDTO.longitude !== undefined)
+    @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.longitude !== undefined || getLocationsDTO.radiusInKilometers !== undefined)
     @Type(() => Number)
-    @ApiProperty()
+    @ApiProperty({
+        name: 'latitude',
+        required: false,
+        description: 'The latitude of a location up to 15 decimal digits. Required for longitude and radiusInKilometers.',
+        type: Number
+    })
     public latitude?: number;
 
     @IsNumber()
@@ -30,24 +47,44 @@ export class GetLocationsDTO {
     @IsLongitude()
     @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.latitude !== undefined || getLocationsDTO.radiusInKilometers !== undefined)
     @Type(() => Number)
-    @ApiProperty()
+    @ApiProperty({
+        name: 'longitude',
+        required: false,
+        description: 'The longitude of a location up to 15 decimal digits. Required for latitude and radiusInKilometers.',
+        type: Number
+    })
     public longitude?: number;
 
     @IsNumber()
     @ValidateIf((getLocationsDTO: GetLocationsDTO) => getLocationsDTO.latitude !== undefined || getLocationsDTO.longitude !== undefined)
     @Type(() => Number)
-    @ApiProperty()
+    @ApiProperty({
+        name: 'radiusInKilometers',
+        required: false,
+        description: 'Filter radius in kilometers. Required for latitude and longitude.',
+        type: Number
+    })
     public radiusInKilometers?: number;
 
     @IsInt()
     @IsOptional()
     @Type(() => Number)
-    @ApiProperty()
+    @ApiProperty({
+        name: 'skip',
+        required: false,
+        description: 'Number of entities to skip.',
+        type: Number
+    })
     public skip?: number;
 
     @IsInt()
     @IsOptional()
     @Type(() => Number)
-    @ApiProperty()
+    @ApiProperty({
+        name: 'take',
+        required: false,
+        description: 'Number of entities to take.',
+        type: Number
+    })
     public take?: number;
 }
